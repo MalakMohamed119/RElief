daca0191f6aca2ac314f920dce2a85c21aae0e9f
Malak Mohamed Mostafa <malakmohmed119.2004@gmail.com>
2026-05-03 21:15:35 +0300

    befor server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://3.99.158.214:5000',
+  apiUrl: 'http://15.223.216.199:5000',
   /**
    * Backend missing forgot-password/reset endpoints.
    * Set to [] to short-circuit frontend.
